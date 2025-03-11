1dbf65b5d02bee94dc985a291483b09052fbc710
kikuchan-code <131835775+kikuchan-code@users.noreply.github.com>
2025-03-11 11:11:37 +0900

    行けてほしいな

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //serviceworkerの設定
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/service-worker.js')
+        navigator.serviceWorker.register('/sw.js')
             .then((registration) => {
                 console.log('Service Worker registered with scope:', registration.scope);
             })
